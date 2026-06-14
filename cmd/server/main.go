f93ebe7decd8086360357d76438892183db0c983
mumutugi <wangibrian66@gmail.com>
2026-06-14 13:45:44 +0300

    feat: Implemented project entry point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"fmt"
+	"tcpchat/internal/chat"
+	"tcpchat/pkg/config"
+)
+
+func main() {
+	cfg, ok := config.MustParse()
+	if !ok {
+		return
+	}
+
+	if err := chat.ValidatePort(cfg.Port); err != nil {
+		fmt.Println(config.Usage)
+		return
+	}
+
+	server := chat.NewServer(cfg.Port)
+	if err := server.Start(); err != nil {
+		fmt.Println(err)
+	}
+}
